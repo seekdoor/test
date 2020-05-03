bb9e06f6ab0342fecf269f0c79a5cd1bc946601f
unknown <36227022+read3@users.noreply.github.com>
2020-05-03 22:29:40 +0800

    dir files encode conversion

---
+++ file-encode-conversion/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const iconv = require('iconv-lite');
+
+let mkdirIfNotExists = (path) => {
+        try {
+          if (!fs.existsSync(path)) fs.mkdirSync(path);
+          fs.accessSync(path, fs.constants.R_OK | fs.constants.W_OK);
+          //console.log('can read/write');
+        } catch (err) {
+          console.error(`no access to ${path}!`);
+        }
+      }
+
+let convDirSync = (pathA,pathB)=>{
+    mkdirIfNotExists(pathB);
+    
+    let items = fs.readdirSync(pathA, {withFileTypes:true});
+
+    for(let item of items){
+        let pathLeft = pathA + '/' + item.name;
+        let pathRight = pathB + '/' + item.name;
+        if(item.isFile()){
+            let contentBuf = fs.readFileSync(pathLeft);
+            let content = iconv.decode( contentBuf, 'GB2312');
+            
+            let convBuf = iconv.encode(content, 'utf8');
+            
+            fs.writeFileSync(pathRight, content);
+        } 
+        else if(item.isDirectory()) convDirSync(pathLeft, pathRight);
+    }
+}
+
+convDirSync('testA', 'testB')
